d8038d6a53fe134de9f33c99388b58ab9577b5d9
ssominsung-afk <ssominsung@gmail.com>
2025-12-26 16:46:55 -0500

    Update company phone number

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,7 @@ export const COMPANY_EMAILS = {
     SALES: "sales@a3pallet.com",
     SUPPORT: "sales@a3pallet.com"
 };
-export const PHONE_NUMBER = "(xxx) xxx-xxxx";
+export const PHONE_NUMBER = "(470) 962-7000";
 export const ADDRESS = "Serving the Southeastern United States";
 
 export const NAV_LINKS = [
